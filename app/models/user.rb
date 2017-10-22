18b36914b7835b0e0919fa92fe6e7b23b45403ec
iammikeyoung <myoung10@gmail.com>
2017-10-22 02:49:14 -0400

    user model implemented

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :first_name,      length: { maximum: 25 }
+  validates :last_name,       length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,           presence: true,
+                              length: { maximum: 50 },
+                              format: { with: VALID_EMAIL_REGEX },
+                              uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true,
+                              length: { minimum: 8 },
+                              allow_nil: true
+
+  def name
+    [first_name, last_name].join " "
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe User do
+  # it { should have_many :portfolios }
+
+  it { should have_valid(:email).when("name@example.com") }
+  it { should_not have_valid(:email).when(nil, "") }
+  it "does not allow duplicate emails in user table" do
+    User.create!(email: "name@example.com")
+    user = User.new
+    user.email = "name@example.com"
+    expect(user).to_not be_valid
+  end
+
+  it "is valid with valid attributes"
+
+  it "returns a contact's full name as a string" do
+    user = User.create!(email: "name@example.com", first_name: "Sterling", last_name: "Archer")
+    expect(user.name).to eq("Sterling Archer")
+  end
+
+end
