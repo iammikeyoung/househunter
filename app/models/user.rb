420231a7846a7ac2d7de3294683de0595a3866a9
iammikeyoung <myoung10@gmail.com>
2017-10-22 13:59:01 -0400

    Implement User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  
-  validates :first_name,      length: { maximum: 25 }
-  validates :last_name,       length: { maximum: 25 }
+
+  has_secure_password
+
+  validates :first_name,  length: { maximum: 25 }
+  validates :last_name,   length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,           presence: true,
-                              length: { maximum: 50 },
-                              format: { with: VALID_EMAIL_REGEX },
-                              uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true,
-                              length: { minimum: 8 },
-                              allow_nil: true
+  validates :email,       presence: true,
+                          length: { maximum: 50 },
+                          format: { with: VALID_EMAIL_REGEX },
+                          uniqueness: { case_sensitive: false }
+  validates :password,    presence: true,
+                          length: { minimum: 8 },
+                          allow_nil: true
 
   def name
     [first_name, last_name].join " "
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe User do
   # it { should have_many :portfolios }
 
+  it "is valid with valid attributes"
+
   it { should have_valid(:email).when("name@example.com") }
   it { should_not have_valid(:email).when(nil, "") }
   it "does not allow duplicate emails in user table" do
-    User.create!(email: "name@example.com")
+    User.create!(email: "name@example.com", password: "password")
     user = User.new
     user.email = "name@example.com"
     expect(user).to_not be_valid
@@ -14,10 +16,17 @@ describe User do
     expect(user).to_not be_valid
   end
 
-  it "is valid with valid attributes"
+  it { should_not have_valid(:password).when(nil, "") }
+  it { should_not have_valid(:password).when("1234567") }
+  it "authenticates a user" do
+    user = User.create!(email: "name@example.com", password: "password")
+    expect(user.authenticate("not_the_right_password")).to eq(false)
+    expect(user.authenticate("password")).to eq(user)
+    expect(!!user.authenticate("password")).to eq(true)
+  end
 
   it "returns a contact's full name as a string" do
-    user = User.create!(email: "name@example.com", first_name: "Sterling", last_name: "Archer")
+    user = User.create!(email: "name@example.com", password: "password", first_name: "Sterling", last_name: "Archer")
     expect(user.name).to eq("Sterling Archer")
   end
 
