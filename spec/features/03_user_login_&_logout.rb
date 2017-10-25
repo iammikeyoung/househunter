96a2f21fc84ba666cbf3ba08cd9e630e5aa07fef
iammikeyoung <myoung10@gmail.com>
2017-10-24 23:20:04 -0400

    spec files for login logout update and delete

---
+++ spec/features/03_user_login_&_logout.rb
@@ -0,0 +1,85 @@
+require "rails_helper"
+
+feature "User Log In", %{
+  As a registered and unauthenticated user
+  I want to login
+  To be an authenticated user & access my account
+
+  Acceptance Criteria:
+  [ ] User must submit an email address
+  [ ] User must submit their password
+  [ ] Email address submitted must be in the database (Users table)
+  [ ] Password submitted must match the hashed password digest in database (Users table)
+
+} do
+
+  scenario "user can access log in page" do
+    visit root_path
+    click_link 'Log In'
+    expect(page).to have_current_path("/login")
+    expect(page).to have_content("Cancel")
+  end
+
+  scenario "Unregistered user attempts to login" do
+    # [ ] Error message of reasons for failure to login
+    # [ ] User remains on login page
+    # [ ] User remains unauthenticated
+
+    visit root_path
+    click_link 'Log In'
+    fill_in "email",    with: "joe_schmoe@email.com"
+    fill_in "password", with: "supersecret"
+    click_button "Log In"
+
+    expect(page).to have_content("error message")
+    expect(page).to have_current_path("/login")
+  end
+
+  scenario "Registered user enters incorrect passwod" do
+    # [ ] Error message of reasons for failure to login
+    # [ ] User remains on login page
+    # [ ] User remains unauthenticated
+
+    visit root_path
+    click_link 'Log In'
+    fill_in "email",    with: "duchess@example.gov"
+    fill_in "password", with: "incorrect"
+    click_button "Log In"
+
+    expect(page).to have_content("error message")
+    expect(page).to have_current_path("/login")
+  end
+
+
+  scenario "Login successful"
+    # [ ] Confirmation message that my account has been logged into
+    # [ ] User is directed to user page
+    # [ ] Login button is no longer available
+    # [ ] Logout button is available
+
+    visit root_path
+    click_link 'Log In'
+    fill_in "email",    with: "duchess@example.gov"
+    fill_in "password", with: "sploosh"
+    click_button "Log In"
+
+    expect(page).to have_content("You have successful logged in")
+    expect(page).to have_current_path("/user/user-id")
+  end
+
+feature "User Log Out", %{
+  As an authenticated user
+  I want to logout
+  To be unauthenticated
+
+  Acceptance Criteria:
+  [ ] Confirmation message that identity has been forgotten on machine I'm using
+  [ ] User is no longer authenticated
+  [ ] No more logout button
+  [ ] Login button should be back
+
+} do
+
+  scenario "user successfully logout"
+
+end
---
+++ spec/features/04_User_update_&_delete_account.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature "User Updates Account". %{
+  As an authenticated user
+  I want to update my information
+  So that I can keep my profile info correct
+
+  Acceptance Criteria:
+  [ ] Update first Name & last Name
+  [ ] Update email with valid email address
+  [ ] Update password with valid password
+
+} do
+
+  scenario "user enters invalid information"
+
+  scenario "user enters valid information"
+
+end
+
+feature "User Deletes Account", %{
+  As an authenticated user
+  I want to delete my account
+  So that my information is no longer retained by the app
+
+  Acceptance Criteria:
+  [ ] User can no longer login with email address & password
+  [ ] User deleted from Users table
+  [ ] User notes are deleted from Notes table
+  [ ] If user is only person associated with Portfolio
+    [ ] Delete portfolio & houses in portfolio
+
+} do
+
+  scenario "user successfully deletes account"
+
+end
