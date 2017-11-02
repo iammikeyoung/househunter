cf2b10c3edc49a3a0d81c80b2d3604cc6588abb9
iammikeyoung <myoung10@gmail.com>
2017-11-01 23:05:04 -0400

    add new house feature complete

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -10,6 +10,21 @@ class HousesController < ApplicationController
 
   def create
     @house = current_user.houses.build(house_params)
+    if @house.save
+      flash[:notice] = "House successfully added"
+      redirect_to current_user
+    else
+      render :new
+    end
   end
+
+  private
+
+    def house_params
+      params.require(:house).permit(:name, :street, :city, :state, :zip_code,
+                                    :asking_amount,
+                                    :total_sqft,
+                                    :photo)
+    end
 
 end
--- spec/features/06_user_can_add_houses_spec.rb
+++ spec/features/06_user_can_add_houses_spec.rb
@@ -24,35 +24,41 @@ feature "Authorized User Adds Houses",
       fill_in "Email",    with: "archer@example.gov"
       fill_in "Password", with: "sploosh1"
       click_button "Log In"
-      click_link("Add House")
+      click_link("Add New House")
 
       expect(page).to have_content("New House Form")
       expect(page).to have_link("Cancel")
       fill_in "Zip Code", with: "02110A"
-      click_button "Add House"
+      click_button "Save"
 
-      expect(page).to have_content("Street Error Msg")
-      expect(page).to have_content("City Error Msg")
-      expect(page).to have_content("State Error Msg")
-      expect(page).to have_content("Zip Code Error Msg")
+      expect(page).to have_content("Street can't be blank")
+      expect(page).to have_content("City can't be blank")
+      expect(page).to have_content("State can't be blank")
+      expect(page).to have_content("Zip code is the wrong length (should be 5 characters)")
     end
 
     scenario "user successfully adds a new house" do
+      user = User.create!(email: "archer@example.gov",
+                          password: "sploosh1",
+                          password_confirmation: "sploosh1",
+                          first_name: "Sterling",
+                          last_name: "Archer")
+
       visit root_path
       click_link 'Log In'
       fill_in "Email",    with: "archer@example.gov"
       fill_in "Password", with: "sploosh1"
       click_button "Log In"
-      click_link("Add New House")
+      click_link "Add New House"
 
-      expect(page).to have_current_path("/")
+      expect(page).to have_content("New House Form")
       expect(page).to have_link("Cancel")
-      fill_in "Name", with: "Around Corner From Mom"
-      fill_in "Name", with: "100 Street"
-      fill_in "Name", with: "City-Name"
-      fill_in "Name", with: "State-Name"
-      fill_in "Name", with: "01234"
-      click_button "Add House"
+      fill_in "Nickname", with: "Around Corner From Mom"
+      fill_in "Street", with: "100 Street"
+      fill_in "City", with: "City-Name"
+      fill_in "State", with: "State-Name"
+      fill_in "Zip Code", with: "01234"
+      click_button "Save"
 
       expect(page).to have_content("House successfully added")
       expect(page).to have_link("Around Corner From Mom")
