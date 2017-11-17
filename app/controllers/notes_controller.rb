6295c4f5b61472c1eb37be684c2ec40d1f4f4992
iammikeyoung <myoung10@gmail.com>
2017-11-17 02:20:53 -0500

    user can add note to house

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,4 +4,26 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
   end
 
+  def new
+    @house = House.find(params[:house_id])
+    @note = Note.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @note = @house.notes.build(note_params) # is there a way to include user_id in this?
+    @note.user = current_user
+    if @note.save
+      flash[:notice] = "Note successfully added!"
+      redirect_to @house
+    else
+      render :new
+    end
+  end
+
+  private
+    def note_params
+      params.require(:note).permit(:room, :rating, :pros, :cons)
+    end
+
 end
--- spec/features/09_user_can_add_notes_spec.rb
+++ spec/features/09_user_can_add_notes_spec.rb
@@ -2,22 +2,81 @@ require 'rails_helper'
 
 feature "authorized user can add a new note to a house in their portfolio" do
 
-  scenario "user provides valid information for new note"
-  # log in
-  # navigate to home
-  # navigate to notes for that house
-  # 'add a new note' button
-  # fill in new note form correctly
-  # expect to be back at house's show page w/ the new note included in page content
+  scenario "user provides valid information for new note" do
+    user = User.create!(email: "archer@example.gov",
+                        password: "sploosh1",
+                        password_confirmation: "sploosh1",
+                        first_name: "Sterling",
+                        last_name: "Archer")
 
-  scenario "user provides invalid information for new note"
-  # log in
-  # navigate to home
-  # navigate to notes for that house
-  # 'add a new note' button
-  # fill in new note form with incorrect data
+    house_one = House.create!(user: user,
+                              name: "Near Work",
+                              street: "101 Arch Street",
+                              city: "Boston",
+                              state: "MA",
+                              zip_code: "02110")
 
-  # stay on new note page
-  # error messages regarding the incorrect data  
+    living_room = Note.create!(house: house_one,
+                              user: user,
+                              room: "Living Room",
+                              rating: 1,
+                              pros: "open floor-plan layout and great for entertaining",
+                              cons: "no fireplace")
+
+    visit root_path
+    click_link 'Log In'
+    fill_in "Email",    with: "archer@example.gov"
+    fill_in "Password", with: "sploosh1"
+    click_button "Log In"
+    click_link 'Near Work'
+    click_link 'Add Room'
+
+    expect(page).to have_current_path("/houses/#{house_one.id}/notes/new")
+    expect(page).to have_content("New Room Form")
+    expect(page).to have_link 'Cancel'
+
+    fill_in "Room",   with: "Kitchen"
+    select "Dislike", from: "Rating"
+    fill_in "Pros",   with: "lots of cabinet space and good flow into sun room"
+    fill_in "Cons",   with: "gallery style - too small"
+    click_button "Save"
+
+    expect(page).to have_content("Note successfully added!")
+    expect(page).to have_content("Kitchen")
+  end
+
+  scenario "user provides invalid information for new note" do
+    user = User.create!(email: "archer@example.gov",
+                        password: "sploosh1",
+                        password_confirmation: "sploosh1",
+                        first_name: "Sterling",
+                        last_name: "Archer")
+
+    house_one = House.create!(user: user,
+                              name: "Near Work",
+                              street: "101 Arch Street",
+                              city: "Boston",
+                              state: "MA",
+                              zip_code: "02110")
+
+    living_room = Note.create!(house: house_one,
+                              user: user,
+                              room: "Living Room",
+                              rating: 1,
+                              pros: "open floor-plan layout and great for entertaining",
+                              cons: "no fireplace")
+
+    visit root_path
+    click_link 'Log In'
+    fill_in "Email",    with: "archer@example.gov"
+    fill_in "Password", with: "sploosh1"
+    click_button "Log In"
+    click_link 'Near Work'
+    click_link 'Add Room'
+    click_button "Save"
+
+    expect(page).to have_content("New Room Form")
+    expect(page).to have_content("Room can't be blank")
+  end
 
 end
