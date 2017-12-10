51085ea953480973a296acd45e8a9ecaee66f641
iammikeyoung <myoung10@gmail.com>
2017-12-10 16:13:52 -0500

    image validations added to model + carrierwave

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,11 +1,12 @@
 class House < ApplicationRecord
   belongs_to :user
   has_many :notes
+  mount_uploader :house_profile_pic, HouseProfilePicUploader
 
   VALID_ZIP_CODE_REGEX = /\d{5}/
   validates :street, :city, :state, :zip_code, presence: true
   validates :zip_code, length: { is: 5 }, format: { with: VALID_ZIP_CODE_REGEX }
-  mount_uploader :house_profile_pic, HouseProfilePicUploader
+  validate :profile_pic_size
 
   def full_address
     [street, city, state, zip_code].join(' ')
@@ -14,4 +15,13 @@ class House < ApplicationRecord
   def price_per_sqft
     asking_amount / total_sqft
   end
+
+  private
+
+    # Validates the size of an uploaded house profile picture.
+    def profile_pic_size
+      if house_profile_pic.size > 5.megabytes
+        errors.add(:house_profile_pic, "should be less than 5MB")
+      end
+    end
 end
