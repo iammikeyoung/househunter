776e8baceddcac0f345b928e28b0d4f726db224c
iammikeyoung <myoung10@gmail.com>
2017-12-16 16:58:47 -0500

    initial changes to User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,89 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many :houses }
+  it "is valid with a first name, last name, email and password" do
+    user = User.new(
+      first_name: "Sterling",
+      last_name: "Archer",
+      email: "archer@example.gov",
+      password: "sploosh1",
+    )
 
-  it { should have_valid(:email).when("name@example.com") }
-  it { should_not have_valid(:email).when(nil, "") }
-  it "does not allow duplicate emails in user table" do
-    User.create!(email: "name@example.com", password: "password")
-    user = User.new
-    user.email = "name@example.com"
-    expect(user).to_not be_valid
-    user.email = user.email.upcase
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a first name" do
+    user1 = User.new(first_name: nil)
+    user2 = User.new(first_name: "")
+
+    expect(user1).to_not be_valid
+    expect(user2).to_not be_valid
+  end
+
+  it "is invalid without a last name" do
+    user1 = User.new(last_name: nil)
+    user2 = User.new(last_name: "")
+
+    expect(user1).to_not be_valid
+    expect(user2).to_not be_valid
+  end
+
+  it "is invalid without a email name" do
+    user1 = User.new(email: nil)
+    user2 = User.new(email: "")
+
+    expect(user1).to_not be_valid
+    expect(user2).to_not be_valid
+  end
+
+  it "is invalid with a duplicate email address in database" do
+    User.create(
+      first_name: "Sterling",
+      last_name: "Archer",
+      email: "archer@example.gov",
+      password: "sploosh1",
+    )
+
+    user = User.new(
+      first_name: "Sterling",
+      last_name: "Archer",
+      email: "archer@example.gov",
+      password: "sploosh1",
+    )
+
     expect(user).to_not be_valid
   end
 
-  it { should_not have_valid(:password).when(nil, "") }
-  it { should_not have_valid(:password).when("1234567") }
-  it "authenticates a user" do
-    user = User.create!(email: "name@example.com", password: "password")
-    expect(user.authenticate("not_the_right_password")).to eq(false)
-    expect(user.authenticate("password")).to eq(user)
-    expect(!!user.authenticate("password")).to eq(true)
+  it "is invalid without a password" do
+    user1 = User.new(password: nil)
+    user2 = User.new(password: "")
+
+    expect(user1).to_not be_valid
+    expect(user2).to_not be_valid
   end
+
+  it "should be invalid with a password that does not fit regex requirements"
 
   it "returns a contact's full name as a string" do
-    user = User.create!(email: "name@example.com", password: "password", first_name: "Sterling", last_name: "Archer")
+    user = User.new(
+      first_name: "Sterling",
+      last_name: "Archer",
+      email: "archer@example.gov",
+      password: "sploosh1",
+    )
+
     expect(user.name).to eq("Sterling Archer")
   end
 
+  it "should have many houses"
+  it "should authenticate"
+
+  # it { should have_many :houses }
+  # it { should_not have_valid(:password).when("1234567") }
+  # it "authenticates a user" do
+  #   user = User.create!(email: "name@example.com", password: "password")
+  #   expect(user.authenticate("not_the_right_password")).to eq(false)
+  #   expect(user.authenticate("password")).to eq(user)
+  #   expect(!!user.authenticate("password")).to eq(true)
+  # end
 end
