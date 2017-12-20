94a9ebaf9d7df40d3af979931728039646966db6
iammikeyoung <myoung10@gmail.com>
2017-12-19 23:20:21 -0500

    WelcomeController updated

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController do
-
-  describe "GET #index" do
-    it "renders the index template" do
+  describe "#index" do
+    it "responds successfully" do
       get :index
-      expect(response).to render_template("welcome/index")
+      expect(response).to be_success
     end
 
     it "returns a 200 OK status" do
       get :index
-      expect(response).to have_http_status(:ok)
+      expect(response).to have_http_status "200"
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("welcome/index")
     end
   end
-
 end
