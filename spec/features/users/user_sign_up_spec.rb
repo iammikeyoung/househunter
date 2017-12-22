b5f653b69da817fdd4d7be940db129618ebdf509
iammikeyoung <myoung10@gmail.com>
2017-12-21 22:35:15 -0500

    user sign up spec refactored

--- spec/features/users/user_sign_up_spec.rb
+++ spec/features/users/user_sign_up_spec.rb
@@ -19,83 +19,57 @@ feature "User Sign Up", %{
       [ ] User is directed to user page
 } do
 
-  scenario "user on welcome page can access sign up page" do
+  scenario "prospective user enters valid data to create an account" do
     visit root_path
-    click_link 'Sign Up'
-    expect(page).to have_current_path("/users/new")
-    expect(page).to have_content('First Name')
-    expect(page).to have_content('Last Name')
-    expect(page).to have_content('Email')
-    expect(page).to have_content('Password (8 characters minimum)')
-    expect(page).to have_content('Confirmation')
-    # expect(page).to have_content('Profile Picture')
-    expect(page).to have_content('Cancel')
-  end
-
-  scenario "user provides invalid email & password information" do
-    visit root_path
-    click_link 'Sign Up'
-    click_button 'Create Account'
-
-    expect(page).to have_content("errors prohibited this user from being saved:")
-    # expect(page).to have_content("First Name can't be blank")
-    # expect(page).to have_content("Last Name can't be blank")
-    expect(page).to have_content("Email can't be blank")
-    expect(page).to have_content("Password can't be blank")
-  end
-
-  scenario "password fields do not match & email format incorrect" do
-    visit root_path
-    click_on "Sign Up"
-
-    fill_in "First Name",     with: "Joe"
-    fill_in "Last Name",      with: "Shmoe"
-    fill_in "Email",          with: "name@emailcom"
-    fill_in "Password",       with: "supersecret"
-    fill_in "Confirmation",   with: "different"
-    click_on "Create Account"
-
-    expect(page).to have_content "Email is invalid"
-    expect(page).to have_content "Password confirmation doesn't match Password"
-  end
-
-  scenario "email is already registered" do
-    existing_user = User.create(
-      first_name: "Joe",
-      last_name: "Schmoe",
-      email: "joe@email.com",
-      password: "passwordsecret",
-      password_confirmation: "passwordsecret"
-    )
-
-    visit root_path
-    click_on "Sign Up"
-
-    fill_in "First Name", with: existing_user.first_name
-    fill_in "Last Name", with: existing_user.last_name
-    fill_in "Email", with: existing_user.email
-    fill_in "Password", with: existing_user.password
-    fill_in "Confirmation", with: existing_user.password
-    click_on "Create Account"
-
-    expect(page).to have_content "Email has already been taken"
-  end
-
-  scenario "user provides valid and required information" do
-    visit root_path
-    click_link 'Sign Up'
+    within(".nav-desktop") do
+      click_link 'Sign Up'
+    end
 
     fill_in 'First Name', with: 'Arya'
     fill_in 'Last Name', with: 'Stark'
     fill_in 'Email', with: 'arya@winterfell.com'
     fill_in 'Password', with: 'HouseStark4Ever'
     fill_in 'Confirmation', with: 'HouseStark4Ever'
-    click_button 'Create Account'
+    click_button 'Create my account'
 
     expect(page).to have_content("You have signed up successfully.")
-    expect(page).to have_content("User's Homepage")
-    expect(page).to have_content("Welcome Arya! You have signed up successfully.")
+    expect(page).to have_content("Welcome Arya!")
   end
 
+  scenario "prospective user enters invalid data to create an account" do
+    visit root_path
+    within(".nav-desktop") do
+      click_on "Sign Up"
+    end
 
+    fill_in "First Name",     with: "Sterling"
+    fill_in "Last Name",      with: ""
+    fill_in "Email",          with: "archer@example-gov"
+    fill_in "Password",       with: "supersecret"
+    fill_in "Confirmation",   with: "different"
+    click_on "Create my account"
+
+    expect(page).to have_content "errors prohibited this user from being saved"
+    expect(page).to have_content "Last name can't be blank"
+    expect(page).to have_content "Email is invalid"
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+  scenario "prospective user enters email that is already registered" do
+    existing_user = FactoryBot.create(:user)
+
+    visit root_path
+    within(".nav-desktop") do
+      click_on "Sign Up"
+    end
+
+    fill_in "First Name", with: existing_user.first_name
+    fill_in "Last Name", with: existing_user.last_name
+    fill_in "Email", with: existing_user.email
+    fill_in "Password", with: existing_user.password
+    fill_in "Confirmation", with: existing_user.password
+    click_on "Create my account"
+
+    expect(page).to have_content "Email has already been taken"
+  end
 end
